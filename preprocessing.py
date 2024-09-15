969beb643d41ae626edb087c817b00c095f7965d
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-09-15 14:27:40 -0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,19 +1,16 @@
 from src.processing import check_coco_data
+from src.download_data import download_data
 
 import os
 import json
 import glob
 
 def main():
-    """
-    Main function to check COCO data in specified directories.
-    """
-    # Directories to check
-    directories = ['data/train', 'data/valid', 'data/test']
+
+      # 
     
-    for directory in directories:
-        print(f"Checking directory: {directory}")
-        check_coco_data(directory)
+      # Download the data from Roboflow
+      download_data()
 
 if __name__ == "__main__":
     main()
