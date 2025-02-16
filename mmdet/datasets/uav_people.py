db05dd348e9776e4cb44eff9b72cfe9da8067aa7
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2025-02-16 14:05:07 -0500

    Update uav_people.py

--- mmdet/datasets/uav_people.py
+++ mmdet/datasets/uav_people.py
@@ -203,23 +203,6 @@ class UAV_People(CustomDataset):
                 json_results.append(data)
         return json_results
 
-    # def _det2json(self, results):
-    #     """Convert detection results to COCO json style."""
-    #     json_results = []
-    #     for idx in range(len(self)):
-    #         img_id = self.img_ids[idx]
-    #         result = results[idx]
-    #         for label in range(len(result)):
-    #             bboxes = result[label]
-    #             for i in range(bboxes.shape[0]):
-    #                 data = dict()
-    #                 data['image_id'] = img_id
-    #                 data['bbox'] = self.xyxy2xywh(bboxes[i])
-    #                 data['score'] = float(bboxes[i][4])
-    #                 data['category_id'] = self.cat_ids[label]
-    #                 json_results.append(data)
-    #     return json_results
-
     def _det2json(self, results):
         """Convert detection results to COCO json style."""
         json_results = []
@@ -236,6 +219,23 @@ class UAV_People(CustomDataset):
                     data['category_id'] = self.cat_ids[label]
                     json_results.append(data)
         return json_results
+
+    # def _det2json(self, results):
+    #     """Convert detection results to COCO json style."""
+    #     json_results = []
+    #     for idx in range(len(self)):
+    #         img_id = self.img_ids[idx]
+    #         result = results[idx]
+    #         for label in range(len(result)):
+    #             bboxes = result[label]
+    #             for i in range(bboxes.shape[0]):
+    #                 data = dict()
+    #                 data['image_id'] = img_id
+    #                 data['bbox'] = self.xyxy2xywh(bboxes[i])
+    #                 data['score'] = float(bboxes[i][4])
+    #                 data['category_id'] = self.cat_ids[label]
+    #                 json_results.append(data)
+    #     return json_results
     
     def _segm2json(self, results):
         """Convert instance segmentation results to COCO json style."""
