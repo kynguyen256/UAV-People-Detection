49b933460c2a22491dded903fb70611b94b24d50
kynguyen256 <163465305+kynguyen256@users.noreply.github.com>
2025-04-15 16:49:00 -0700

    Update download_IR.py
    fix re-downloading dataset unnecessarily every single time

--- src/download_IR.py
+++ src/download_IR.py
@@ -1,24 +1,22 @@
 import os
 
 def download_IR():
-    # Create a data/ directory if it doesn't exist
-
+    # Define the parent data directory
     parent_dir = "data"
     if not os.path.exists(parent_dir):
         os.makedirs(parent_dir)
 
-    # Define the RGB directory inside data/
+    # Define the IR directory inside data/
     data_dir = os.path.join(parent_dir, "IR")
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
 
-    # Define the paths for the extracted folders
+    # Define the paths for the extracted folders (only train and valid for IR dataset)
     train_folder = os.path.join(data_dir, "train")
-    test_folder = os.path.join(data_dir, "test")
     valid_folder = os.path.join(data_dir, "valid")
 
-    # Check if the dataset is already extracted (i.e., train, test, and valid folders exist)
-    if not (os.path.exists(train_folder) and os.path.exists(test_folder) and os.path.exists(valid_folder)):
+    # Check if the dataset is already extracted (i.e., train and valid folders exist)
+    if not (os.path.exists(train_folder) and os.path.exists(valid_folder)):
         # Define the path for the dataset zip file
         dataset_path = os.path.join(data_dir, "roboflow.zip")
 
@@ -28,14 +26,14 @@ def download_IR():
             os.system(f'curl -L "https://universe.roboflow.com/ds/E9VjZ9kO6s?key=4FEt7LlMv0" > {dataset_path}')
             print("IR dataset downloaded successfully.")
 
-        # Unzip the dataset
-        os.system(f'unzip {dataset_path} -d {data_dir}')
+        # Unzip the dataset with overwrite
+        os.system(f'unzip -o {dataset_path} -d {data_dir}')
         
         # Remove the ZIP file
         os.remove(dataset_path)
         print("IR dataset extracted successfully.")
     else:
-        print("IR dataset already exists with 'train', 'test', and 'valid' folders.")
+        print("IR dataset already exists with 'train' and 'valid' folders.")
 
 # Call the function to download the new dataset
 if __name__ == "__main__":
